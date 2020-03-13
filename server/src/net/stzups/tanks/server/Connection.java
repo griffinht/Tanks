5f2344bb69dbfa980e3bfebbdcd4f0b51e5a624d
stzups <lemonpickles1558@gmail.com>
2020-03-12 21:40:34 -0400

    Organized methods, better error handling

--- server/src/net/stzups/tanks/server/Connection.java
+++ server/src/net/stzups/tanks/server/Connection.java
@@ -3,7 +3,6 @@ package net.stzups.tanks.server;
 import net.stzups.tanks.FileManager;
 import net.stzups.tanks.Tanks;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -99,55 +98,36 @@ public class Connection implements Runnable {
                         outputStream.write(response, 0, response.length);
 
                         logger.info("Client connected from IP address " + socket.getInetAddress().getHostAddress());
+
                         connection:
                         while(connected) {
                             if (inputStream.available() > 0) {
                                 byte[] head = new byte[2];
-                                inputStream.read(head, 0, 2);
-                                if (((head[0] >> 7) & 1 ) != 1) { // FIN bit todo handle non FIN
-                                    logger.info("not FIN");
-                                }
-                                byte[] decoded;
-                                if (((head[1] >> 7) & 1) == 1) { // Mask bit
-                                    switch (head[1] + 0x80) {
-                                        case 126:
-                                            inputStream.skip(2); //todo read these?
-                                            break;
-                                        case 127:
-                                            inputStream.skip(4);
-                                            break;
-                                    }
-                                    byte[] key = new byte[4];
-                                    inputStream.read(key, 0, 4);
-                                    byte[] encoded = new byte[inputStream.available()];
-                                    decoded = new byte[encoded.length];
-                                    inputStream.read(encoded);
-                                    for (int i = 0; i < encoded.length; i++) {
-                                        decoded[i] = (byte) (encoded[i] ^ key[i & 0x3]);
-                                    }
-                                } else {
-                                    byte[] packet = new byte[inputStream.available()];
-                                    inputStream.read(packet);
-                                    throw new RuntimeException("Received unmasked data from client " + uuid + ", full packet: " + readBytesToString(head) + " " + readBytesToString(packet));
+
+                                if (inputStream.read(head) != 2)
+                                    throw new IOException();
+
+                                byte[] decoded = decodePayload(head, inputStream);
+
+                                if (((head[0] >> 7) & 1 ) != 1) {
+                                    throw new RuntimeException("Client sent non fin bit indicating fragmented message, server can't handle");
                                 }
 
                                 switch (head[0] & 0x0F) { // Opcode bits
                                     case 0x0: // continuation frame
-                                        logger.info("continuation frame");
-                                        break;
+                                        throw new RuntimeException("Client sent continuation frame, server can't handle");
                                     case 0x1: // text frame
                                         server.onTextPacket(this, new String(decoded));
                                         break;
                                     case 0x2: // binary frame
-                                        logger.info("binary frame");
+                                        logger.info("binary frame"); //todo handle
                                         break;
                                     case 0x8: // connection close
                                         logger.info("Client disconnected from IP address " + socket.getInetAddress().getHostAddress());
                                         close(true);
                                         break connection;
                                     case 0x9: // ping, shouldn't ever receive one
-                                        logger.info("ping");
-                                        break;
+                                        throw new RuntimeException("Client sent ping, server can't handle");
                                     case 0xA: // pong from client
                                         long time = System.currentTimeMillis();
                                         ping = (int) (time - lastPing);
@@ -156,8 +136,9 @@ public class Connection implements Runnable {
                                         break;
                                     default: // error
                                         byte[] packet = new byte[inputStream.available()];
-                                        inputStream.read(packet);
-                                        throw new RuntimeException("Unrecognized opcode ( "+ readBytesToString((byte) (head[0] & 0x0F)).substring(4) + " ) from client " + getUUID() + ", full packet: " + readBytesToString(head) + " " + readBytesToString(packet));
+                                        if (inputStream.read(packet) == -1)
+                                            throw new IOException("Could not read malformed packet");
+                                        throw new RuntimeException("Unrecognized opcode ( "+ readBytesToString((byte) (head[0] & 0x0F)).substring(4) + " ) from client " + getUUID() + ", full head packet: " + readBytesToString(head) + " ");
                                 }
                             }
 
@@ -209,7 +190,72 @@ public class Connection implements Runnable {
         }
     }
 
-    static private byte[] getFramedPacket(byte opcode, String payload) {
+    private static int readLength(byte[] head, InputStream inputStream) throws IOException {
+        byte[] lengthBits;
+        int length;
+        switch (head[1] + 0x80) {
+            case 126:
+                lengthBits = new byte[2];
+                if (inputStream.read(lengthBits) != 2)
+                    throw new IOException();
+                length = (((lengthBits[0] & 0xFF) << 8)
+                        | (lengthBits[1] & 0xFF));
+                break;
+            case 127:
+                throw new RuntimeException("Client sent message too long, server can't handle");
+                /*
+                lengthBits = new byte[8];
+                if (inputStream.read(lengthBits) != 8)
+                    throw new IOException();
+                length = (((long)(lengthBits[0] & 0xFF) << 56)
+                        | ((long)(lengthBits[1] & 0xFF) << 48)
+                        | ((long)(lengthBits[2] & 0xFF) << 40)
+                        | ((long)(lengthBits[3] & 0xFF) << 32)
+                        | ((long)(lengthBits[4] & 0xFF) << 24)
+                        | ((long)(lengthBits[5] & 0xFF) << 16)
+                        | ((long)(lengthBits[6] & 0xFF) << 8)
+                        | ((long)lengthBits[7] & 0xFF));
+                break;
+                 */
+            default:
+                length = head[1] + 0x80;
+        }
+
+        return length;
+    }
+
+    private static byte[] decodePayload(byte[] head, InputStream inputStream) throws IOException {
+        byte[] decoded;
+
+        if (((head[1] >> 7) & 1) == 1) { // Mask bit
+            long length = readLength(head, inputStream);
+
+            byte[] key = new byte[4];
+            if (inputStream.read(key) != 4)
+                throw new IOException("Couldn't read masking bits");
+            byte[] encoded = new byte[inputStream.available()];
+            decoded = new byte[encoded.length];
+
+            if (inputStream.read(encoded) == -1)
+                throw new IOException("Couldn't read encoded payload");
+            for (int i = 0; i < encoded.length; i++) {
+                decoded[i] = (byte) (encoded[i] ^ key[i & 0x3]);
+            }
+
+            if (length != decoded.length) {
+                throw new RuntimeException("Mismatching payload lengths, (packet length: " + length + ", actual length " + decoded.length + ")");
+            }
+        } else {
+            byte[] packet = new byte[inputStream.available()];
+            if (inputStream.read(packet) == -1)
+                throw new IOException();
+            throw new RuntimeException("Received unmasked data from client, full HEAD packet: " + readBytesToString(head));
+        }
+
+        return decoded;
+    }
+
+    private static byte[] getFramedPacket(byte opcode, String payload) {
         byte[] data;
         int offset;
 
@@ -235,7 +281,7 @@ public class Connection implements Runnable {
         return data;
     }
 
-    static private String readBytesToString(byte[] bytes) {
+    private static String readBytesToString(byte[] bytes) {
         StringBuilder string = new StringBuilder();
 
         for (byte b : bytes) {
@@ -260,7 +306,7 @@ public class Connection implements Runnable {
         }
     }
 
-    static private String readBytesToString(byte b) {
+    private static String readBytesToString(byte b) {
         return readBytesToString(new byte[]{b});
     }
 
