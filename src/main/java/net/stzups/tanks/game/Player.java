7e70431c9bcc009b2556bed38a6e5fc40a68de29
stzups <lemonpickles1558@gmail.com>
2020-03-30 13:00:29 -0400

    add proper viewport calculations

--- src/main/java/net/stzups/tanks/game/Player.java
+++ src/main/java/net/stzups/tanks/game/Player.java
@@ -1,15 +1,29 @@
 package net.stzups.tanks.game;
 
 public class Player extends Entity {
-    private String name;
-    private double viewportX;
-    private double viewportY;
-    private int viewportWidth;
-    private int viewportHeight;
 
-    Player(String name, double x, double y) {
+    private static final float MAX_VIEWPORT_WIDTH = 16;
+    private static final float MAX_VIEWPORT_HEIGHT = 9;
+    private static final int VIEWPORT_SCALE = 4;
+
+    private String name;
+    int viewportWidth;
+    int viewportHeight;
+
+    Player(String name, double x, double y, int viewportWidth, int viewportHeight) {
         super(x, y, 0, 0);
         this.name = name;
+        setViewport(viewportWidth, viewportHeight);
+    }
+
+    private void setViewport(int viewportWidth, int viewportHeight) {
+        if ((float) viewportWidth / (float) viewportHeight >= MAX_VIEWPORT_WIDTH / MAX_VIEWPORT_HEIGHT) {
+            this.viewportWidth = (int) (MAX_VIEWPORT_WIDTH * VIEWPORT_SCALE);
+            this.viewportHeight = (int) ((float) this.viewportWidth * ((float) viewportHeight / (float) viewportWidth));
+        } else {
+            this.viewportHeight = (int) (MAX_VIEWPORT_HEIGHT * VIEWPORT_SCALE);
+            this.viewportWidth = (int) ((float) this.viewportHeight * ((float) viewportWidth / (float) viewportHeight));
+        }
     }
 
     public String getName() {
